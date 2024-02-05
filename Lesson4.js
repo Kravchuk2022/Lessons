a87846096ad436757c089431f7be60607bcdfb1d
Kravchuk2022 <akravchuk2014@gmail.com>
2024-02-06 00:28:24 +0200

    1

--- Lesson4.js
+++ Lesson4.js
@@ -1,4 +1,43 @@
-let i = 0;
-while (i < 3) {
-    console.log('i= ' + i++);
+
+/*let lines = 5;
+let str = " ";
+let symbol = "*";
+
+function drawTriangle (lines,symbol) {
+ for (let d = 0; d < lines; d++) {
+  str += symbol;
+  console.log(str);}
 }
+
+drawTriangle (5,'*');*/
+
+
+/*let lines = 5;
+let symbol = "*";
+
+for (let i = 1; i <= lines; i++) {
+  let str = '';
+  for (let j = 1; j <= i; j++) {
+    str += symbol;
+  }
+  console.log(str);
+}*/
+
+/*let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 !== 0) {
+    sum += i;
+  }
+}
+console.log(sum);*/
+
+/*let x = 2;
+let y = 3;
+let result = 1;
+
+for (let i = 0; i < y; i++) {
+  result *= x;
+}
+
+console.log(result);*/
+
